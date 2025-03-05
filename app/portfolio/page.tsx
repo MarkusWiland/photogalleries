5e092d6ff34a95aecc1d0eac661f983d1845d1d0
Markus Wiland <markus.wiland@outlook.com>
2025-03-05 09:34:09 +0100

    create folder

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,23 +1,21 @@
-'use client'
-import AllFolders from '@/components/global/AllFolders'
-import { Modal } from '@/components/global/Modal'
-import { Button } from '@/components/ui/button'
-import { useStoreModal } from '@/hooks/use-store-modal'
+import { FolderForm } from "@/components/Forms/FolderForm";
+import AllFolders from "@/components/global/AllFolders";
+import { Modal } from "@/components/global/Modal";
 
 export default function Portfolio() {
-
   return (
     <div className="container mx-auto">
       <div className="flex justify-between items-center mb-8">
-        <h1 className="text-2xl font-bold">DIn portfolio</h1>
+        <h1 className="text-2xl font-bold">Din portfolio</h1>
 
-        <Modal title="he" description="hej" btnText='Skapa Katalog'>
-          <Button>hej</Button>
+        <Modal title="Skapa ny katalog" description="Fyll i detaljer för din katalog" btnText="Skapa Katalog">
+          <FolderForm />
         </Modal>
       </div>
+
       <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
-     
+        <AllFolders />
       </div>
     </div>
-  )
+  );
 }
